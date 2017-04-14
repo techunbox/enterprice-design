721c2cd3da6f654846ac0d8f690682ae940b62de
Vinod Sharma <vinod@edorei.com>
2017-04-14 21:48:03 +0530

    entity

---
+++ ecom-entity/src/main/java/pro/techunbox/entity/ecom/App.java
@@ -0,0 +1,13 @@
+package pro.techunbox.entity.ecom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
